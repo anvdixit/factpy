9cafccf39dc5473d741288878ec5f14e9f627dd3
anvdixit <39910276+anvdixit@users.noreply.github.com>
2018-06-11 12:19:34 -0700

    Add files via upload

---
+++ code1.py
@@ -0,0 +1,17 @@
+#calculating factorial of a number
+
+#variable to take input from user
+number=raw_input("Enter a number to calculate its factorial ")
+num=int(number)
+
+fact=1
+while num>1 :	
+	fact=fact*num
+	num=num-1
+print(" Factorial is ", fact)
+
+
+
+
+
+	
